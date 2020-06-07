31a8ef8a2612395ec7d7499d5c727dde024da2b8
seroujkh <rafayel28@hotmail.com>
2020-06-07 18:55:43 +0300

    heroku publish

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,9 @@ weatherForm.addEventListener('submit', (e) => {
     m1.textContent="Loading..."
     m2.textContent=""
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=> {
+
+
+    fetch('/weather?address='+location).then((response)=> {
        
     response.json().then( (data) => {
 
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const hbs = require('hbs')
 
 const app = express()
 
+const port = process.env.PORT ||3000
+
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -97,6 +99,8 @@ app.get('*',(req,res)=>{
     })
 
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+
+//port val heroku
+app.listen(port, () => {
+    console.log('Server is up on port .'+ port)
 })
